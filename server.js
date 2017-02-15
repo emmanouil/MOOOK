8cb6e749b7affa08430cf2bfc82bef28999bc532
emmanouil <potetsia@enst.fr>
2017-02-15 15:10:50 +0100

    tidy up server code

--- server.js
+++ server.js
@@ -2,37 +2,39 @@
 "use strict";
 //modules
 var http = require("http"),
-    url = require("url"),
-    path = require("path"),
-    fs = require("fs"),
-    port = process.argv[2] || 8080;
+  url = require("url"),
+  path = require("path"),
+  fs = require("fs"),
+  port = process.argv[2] || 8080;
 //options
-var LOOK_FOR_INDEX = false;	//if url is a dir, try to load index.html in this dir
+var LOOK_FOR_INDEX = true;	//if url is a dir, try to load index.html in this dir
 
-http.createServer(function(request, response) {
+http.createServer(function (request, response) {
 
-  var uri = url.parse(request.url).pathname, filename = path.join(process.cwd(), uri);
-  
-  fs.exists(filename, function(exists) {
-    if(!exists) {
-      response.writeHead(404, {"Content-Type": "text/plain"});
+  var uri = url.parse(request.url).pathname;
+  var filename = path.join(process.cwd(), uri);
+  var file = uri.toString().split('/').pop();
+
+  fs.exists(filename, function (exists) {
+    if (!exists) {
+      response.writeHead(404, { "Content-Type": "text/plain" });
       response.write("404 Not Found\n");
       response.end();
       return;
     }
 
-    if (fs.statSync(filename).isDirectory()){
-		if(LOOK_FOR_INDEX){
-			filename += '/index.html';
-		}else{
-		//TODO
-		}
-			
-	}
+    if (fs.statSync(filename).isDirectory()) {
+      if (LOOK_FOR_INDEX) {
+        filename += '/index.html';
+      } else {
+        //TODO
+      }
 
-    fs.readFile(filename, "binary", function(err, file) {
-      if(err) {        
-        response.writeHead(500, {"Content-Type": "text/plain"});
+    }
+
+    fs.readFile(filename, "binary", function (err, file) {
+      if (err) {
+        response.writeHead(500, { "Content-Type": "text/plain" });
         response.write(err + "\n");
         response.end();
         return;
