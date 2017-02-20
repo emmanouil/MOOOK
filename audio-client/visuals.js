b2b44187a5d8c0a1bf58919a867667bdd7dc4901
emmanouil <potetsia@enst.fr>
2017-02-20 16:12:11 +0100

    removed p5.js (and particles)

--- audio-client/visuals.js
+++ audio-client/visuals.js
@@ -1,6 +1,5 @@
 //options
 var WITH_GRADIENT = false;
-var WITH_PARTICLES = false;
 var WITH_SKELETON = true;
 
 //vars
@@ -18,19 +17,13 @@ var lastSkelDel =[];
 var lastSkelProj =[];
 
 function canvasInit() {
-	setup();
-	canvas = document.querySelector('.canvas');
+	canvas = document.createElement('canvas');
+	canvas.id = ('c')
+	document.body.appendChild(canvas);
+	//canvas = document.querySelector('.canvas');
 	canvas.width = video.width;
 	canvas.height = video.height;
 	canvasCtx = canvas.getContext('2d');
-	system = new ParticleSystem(createVector(width / 2, 50));
-}
-
-function setup() {
-	cp5js = createCanvas(640, 480);
-	console.log(cp5js);
-	cp5js.id = 'c5';
-	return;
 }
 
 function drawViz(e, skel_type) {
@@ -39,7 +32,6 @@ function drawViz(e, skel_type) {
 	}
 	var projC = e.coordsProj;
 
-	clear();
 
 	if (WITH_GRADIENT) {
 
@@ -121,12 +113,8 @@ function do_viz(projC, skel_type) {
 		if (index == 7) {
 			canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
 			slot = 0;
-			if (WITH_PARTICLES)
-				system.addParticle(2 * item[0], 2 * item[1], index);
 		} else if (index == 11) {
 			slot = 1;
-			if (WITH_PARTICLES)
-				system.addParticle(2 * item[0], 2 * item[1], index);
 		} else {
 			return;
 		}
@@ -148,7 +136,6 @@ function do_viz(projC, skel_type) {
 		drawArc(lastDrawnDel[slot], 'rgb(255,0,0)');
 	});
 
-	system.run();
 }
 
 function drawSkeleton(skelArray_in, colour){
@@ -168,72 +155,3 @@ function drawArc(xy, colour) {
 	canvasCtx.fill();
 	canvasCtx.closePath();
 }
-
-var Particle = function (position, x, y, number) {
-	this.acceleration = createVector(0, 0.05);
-	this.velocity = createVector(random(-1, 1), random(-1, 0));
-	if (typeof x === 'undefined') {
-		this.position = position.copy();
-		this.number = -1;
-	} else {
-		this.position = createVector(x, y);
-		this.number = number;
-	}
-	this.lifespan = 120.0;
-};
-
-Particle.prototype.run = function () {
-	this.update();
-	this.display();
-};
-
-// Method to update position
-Particle.prototype.update = function () {
-	this.velocity.add(this.acceleration);
-	this.position.add(this.velocity);
-	this.lifespan -= 2;
-};
-
-// Method to display
-Particle.prototype.display = function () {
-	stroke(200, this.lifespan * 2);
-	strokeWeight(2);
-	if (this.number > 0) {
-		if (this.number == 7) {
-			fill(255, 0, 0, this.lifespan * 2);
-		} else if (this.number == 11) {
-			fill(0, 255, 0, this.lifespan * 2);
-		}
-	} else {
-		fill(127, this.lifespan * 2);
-	}
-	ellipse(this.position.x, this.position.y, 12, 12);
-};
-
-// Is the particle still useful?
-Particle.prototype.isDead = function () {
-	if (this.lifespan < 0) {
-		return true;
-	} else {
-		return false;
-	}
-};
-
-var ParticleSystem = function (position) {
-	//this.origin = position.copy();
-	this.particles = [];
-};
-
-ParticleSystem.prototype.addParticle = function (x, y, number) {
-	this.particles.push(new Particle(this.origin, x, y, number));
-};
-
-ParticleSystem.prototype.run = function () {
-	for (var i = this.particles.length - 1; i >= 0; i--) {
-		var p = this.particles[i];
-		p.run();
-		if (p.isDead()) {
-			this.particles.splice(i, 1);
-		}
-	}
-};
